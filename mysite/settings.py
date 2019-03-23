8971c1a4e0eb5cc81946747144ec59944deba64c
mstewart <meganfstewart@gmail.com>
2019-03-23 15:13:17 -0400

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join (BASE_DIR, 'Static')
 
-ALLOWED_HOSTS=['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS=['127.0.0.1', 'megstew.pythonanywhere.com']
 
 
 
